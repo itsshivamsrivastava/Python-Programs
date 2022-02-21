dfbdddbf11045a9c175bf099dcd6a701fd67962b
Shivam Srivastava <73056786+itsshivamsrivastava@users.noreply.github.com>
2022-02-21 21:42:12 +0530

    Add files via upload

---
+++ turtle_style.py
@@ -0,0 +1,14 @@
+import turtle as tt
+import time
+
+time.sleep(1)
+tt.bgcolor('black')
+tt.color('red','green')
+tt.begin_fill()
+
+for i in range(36):
+    tt.forward(200)
+    tt.left(170)
+tt.end_fill()
+tt.done()
+	
